dde3e549671540d00df065105e685292078931e2
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-04-14 00:17:29 +0200

    personalisation mug en cours

--- src/Controller/PersonalisationMugsController.php
+++ src/Controller/PersonalisationMugsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class PersonalisationMugsController extends AbstractController
 {
     #[Route('/personalisation/mugs', name: 'app_personalisation_mugs')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $mug = $produitRepository->findOneBy(['Categorie' => 1]);
+
+        if(!$mug) {
+            throw $this->createNotFoundException('Prix à définir');
+        }
         return $this->render('personalisation_mugs/index.html.twig', [
             'controller_name' => 'PersonalisationMugsController',
+            'mug' => $mug
         ]);
     }
 }
