82e0e0dd8526d02f4b21a354f46acd4e3f033ea5
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-06-15 00:41:19 +0200

    création modal dynamique dans categorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route; 
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategorieRepository;
+use App\Entity\Categorie;
 
 class CategorieController extends AbstractController
 {
@@ -13,9 +14,19 @@ class CategorieController extends AbstractController
     public function index(CategorieRepository $categorieRepository): Response
     {
         $categories = $categorieRepository->findAll();
-        
+
         return $this->render('categorie/index.html.twig', [
             'categories' => $categories,
         ]);
     }
+
+    #[Route('/categorie/{id}', name: 'app_categorie_show', methods: ['GET'])]
+    public function show(Categorie $categorie): Response
+    {
+        return $this->json([
+            'nom' => $categorie->getNom(),
+            'description' => $categorie->getDescription(),
+        ]);
+    }
 }
+
